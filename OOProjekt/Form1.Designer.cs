96b5a7b03f6cdc09b2a816a999f2b0011488ebf5
FredTheNoob <frederikhammer02@gmail.com>
2019-04-23 12:11:53 +0200

    Finished version 1.0 - add apple to listview

--- OOProjekt/Form1.Designer.cs
+++ OOProjekt/Form1.Designer.cs
@@ -36,11 +36,12 @@
             this.btnMinimize = new System.Windows.Forms.Label();
             this.lvBoks = new System.Windows.Forms.ListView();
             this.clnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clnCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clnPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clnPlu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clnCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
+            this.clnAmount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,26 +49,29 @@
             // 
             this.txtSearch.BackColor = System.Drawing.Color.Silver;
             this.txtSearch.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.txtSearch.Location = new System.Drawing.Point(857, 65);
+            this.txtSearch.Location = new System.Drawing.Point(643, 53);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(142, 22);
+            this.txtSearch.Size = new System.Drawing.Size(108, 20);
             this.txtSearch.TabIndex = 6;
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(798, 65);
+            this.lblSearch.Location = new System.Drawing.Point(598, 53);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(53, 17);
+            this.lblSearch.Size = new System.Drawing.Size(41, 13);
             this.lblSearch.TabIndex = 7;
             this.lblSearch.Text = "Search";
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(467, 43);
+            this.lblTitle.Location = new System.Drawing.Point(350, 35);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(108, 17);
+            this.lblTitle.Size = new System.Drawing.Size(85, 13);
             this.lblTitle.TabIndex = 8;
             this.lblTitle.Text = "Welcome USER";
             // 
@@ -75,8 +79,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1028, 26);
+            this.pictureBox1.Size = new System.Drawing.Size(771, 21);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
@@ -87,9 +92,10 @@
             this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.Color.Silver;
-            this.btnExit.Location = new System.Drawing.Point(992, -4);
+            this.btnExit.Location = new System.Drawing.Point(744, -3);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(39, 29);
+            this.btnExit.Size = new System.Drawing.Size(31, 24);
             this.btnExit.TabIndex = 10;
             this.btnExit.Text = "✖";
             this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
@@ -102,9 +108,10 @@
             this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMinimize.ForeColor = System.Drawing.Color.Silver;
-            this.btnMinimize.Location = new System.Drawing.Point(961, -4);
+            this.btnMinimize.Location = new System.Drawing.Point(721, -3);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(27, 29);
+            this.btnMinimize.Size = new System.Drawing.Size(21, 24);
             this.btnMinimize.TabIndex = 11;
             this.btnMinimize.Text = "−";
             this.btnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
@@ -115,14 +122,16 @@
             // 
             this.lvBoks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.clnName,
+            this.clnAmount,
             this.clnCategory,
             this.clnPrice,
             this.clnPlu});
             this.lvBoks.FullRowSelect = true;
             this.lvBoks.GridLines = true;
-            this.lvBoks.Location = new System.Drawing.Point(25, 98);
+            this.lvBoks.Location = new System.Drawing.Point(19, 80);
+            this.lvBoks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lvBoks.Name = "lvBoks";
-            this.lvBoks.Size = new System.Drawing.Size(974, 463);
+            this.lvBoks.Size = new System.Drawing.Size(732, 377);
             this.lvBoks.TabIndex = 12;
             this.lvBoks.UseCompatibleStateImageBehavior = false;
             this.lvBoks.View = System.Windows.Forms.View.Details;
@@ -130,46 +139,56 @@
             // clnName
             // 
             this.clnName.Text = "Name";
-            this.clnName.Width = 289;
-            // 
-            // clnPrice
-            // 
-            this.clnPrice.Text = "Price";
-            this.clnPrice.Width = 366;
-            // 
-            // clnPlu
-            // 
-            this.clnPlu.Text = "PLU";
+            this.clnName.Width = 167;
             // 
             // clnCategory
             // 
             this.clnCategory.Text = "Category";
-            this.clnCategory.Width = 255;
+            this.clnCategory.Width = 173;
+            // 
+            // clnPrice
+            // 
+            this.clnPrice.Text = "Price";
+            this.clnPrice.Width = 158;
+            // 
+            // clnPlu
+            // 
+            this.clnPlu.Text = "PLU";
+            this.clnPlu.Width = 41;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(864, 580);
+            this.btnAdd.Location = new System.Drawing.Point(648, 471);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(135, 41);
+            this.btnAdd.Size = new System.Drawing.Size(101, 33);
             this.btnAdd.TabIndex = 13;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(25, 580);
+            this.btnRemove.Location = new System.Drawing.Point(19, 471);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(135, 41);
+            this.btnRemove.Size = new System.Drawing.Size(101, 33);
             this.btnRemove.TabIndex = 14;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // clnAmount
+            // 
+            this.clnAmount.Text = "Amount";
+            this.clnAmount.Width = 189;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1027, 650);
+            this.ClientSize = new System.Drawing.Size(770, 528);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lvBoks);
@@ -180,8 +199,10 @@
             this.Controls.Add(this.lblSearch);
             this.Controls.Add(this.txtSearch);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Storer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -202,6 +223,7 @@
         private System.Windows.Forms.ColumnHeader clnPlu;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ColumnHeader clnAmount;
     }
 }
 
--- OOProjekt/Form1.cs
+++ OOProjekt/Form1.cs
@@ -62,5 +62,36 @@ namespace OOProjekt
         {
             btnMinimize.BackColor = pictureBox1.BackColor;
         }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the listbox object called lvItem and insert the first item into column 0 called "apple" (the name column)
+            ListViewItem lvItem = new ListViewItem("Apple");
+            // Move onto the subitem of apple and find column 1 (tabindex 1) and then insert the value 4 into this column (the amount column)
+            lvItem.SubItems.Add("4");
+            // Move onto the subitem of apple and find column 2 (tabindex 2) and then insert the string: fruits into this column (the category column)
+            lvItem.SubItems.Add("Fruits");
+            // Move onto the subitem of apple and find column 3 (tabindex 3) and then insert the string: $0.50 into this column (the price column)
+            lvItem.SubItems.Add("$0.50");
+            // Move onto the subitem of apple and find column 4 (tabindex 4) and then insert the string: 137 into this column (the PLU column)
+            lvItem.SubItems.Add("137");
+            // Add the items into the listview of the variable lvItem
+            lvBoks.Items.Add(lvItem);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            // If the user has not selected anything in the listview (==0) then
+            if (lvBoks.SelectedItems.Count == 0) return;
+            {
+                // Remove the selected item from the listview (using array number 0)
+                lvBoks.SelectedItems[0].Remove();
+            }
+        }
     }
 }
